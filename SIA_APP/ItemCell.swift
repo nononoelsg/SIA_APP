236fa5d124cd9181472b2ab6014ace43b2d886fb
henrylai <henrynus92@gmail.com>
2019-08-09 12:32:07 +0800

    integrate new AR camera code

---
+++ SIA_APP/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  SIA_APP
+//
+//  Created by henrylai on 9/8/19.
+//  Copyright © 2019 spellworks. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
--- SIA_APP/ViewController.swift
+++ SIA_APP/ViewController.swift
@@ -9,37 +9,143 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
+    
+    
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    // Create a session configuration for item image view
+    let configuration = ARImageTrackingConfiguration()
+    let configuration_1 = ARWorldTrackingConfiguration()
+    
+    var selectedItem : String?
     var AppleNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLighting()
+        self.registerGestureRecognizer()
+        imageTrackConfig()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initConfig()
+    }
+    
+    //functionn to initialize the plane detection and image tracking
+    func initConfig() {
+        self.configuration_1.planeDetection = .horizontal
+
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "ARResources", bundle: Bundle.main){
+            configuration.trackingImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 2
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        sceneView.session.run(configuration_1)
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        
+        self.sceneView.delegate = self
+    }
+    
+    // configure the 3D model lighting
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    // track the image function
+    func imageTrackConfig(){
+        //the image tracking configuration
         sceneView.delegate = self
         sceneView.autoenablesDefaultLighting = true
         let appleScene = SCNScene(named: "art.scnassets/AppleStrudel.scn")
         AppleNode = appleScene?.rootNode
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    // detection tap gesture on the iphone 
+    func registerGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
+        longPressGestureRecognizer.minimumPressDuration = 0.1
         
-        // Create a session configuration for world view
-        //let configuration = ARWorldTrackingConfiguration()
+        self.sceneView.addGestureRecognizer(longPressGestureRecognizer)
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         
-        // Create a session configuration for item image view
-        let configuration = ARImageTrackingConfiguration()
-        
-        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "ARResources", bundle: Bundle.main){
-            configuration.trackingImages = trackingImages
-            configuration.maximumNumberOfTrackedImages = 2
-        }
-
-        // Run the view's session
-        sceneView.session.run(configuration)
     }
+    
+    @objc func rotate (sender: UILongPressGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+        if !hitTest.isEmpty {
+            let result = hitTest.first!
+            
+            if sender.state == .began {
+                let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 1)
+                let forever = SCNAction.repeatForever(rotation)
+                result.node.runAction(forever)
+            } else if sender.state == .ended {
+                result.node.removeAllActions()
+            }
+        }
+    }
+    
+    @objc func pinch (sender: UIPinchGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let pinchLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(pinchLocation)
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            let node = results.node
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node.runAction(pinchAction)
+            sender.scale = 1.0
+        }
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(tapLocation)
+        
+        guard  let node = hitTestResult.first?.node else {
+            let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+            
+            if !hitTest.isEmpty {
+                self.addItem(hitTestResult: hitTest.first!)
+            }
+            return
+        }
+        
+        node.removeFromParentNode()
+        
+        
+    }
+    
+    func addItem (hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem {
+            let scene = SCNScene(named: "Assets/\(selectedItem).scn")
+            let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -47,6 +153,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Item", for: indexPath) as! ItemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.blue
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.red
+    }
+    
     
     //render the image
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
@@ -67,4 +195,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         return node
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            }
+        }
+    }
+}
+
+extension Int {
+    var degreesToRadians: Double {return Double(self) * .pi/180}
 }
